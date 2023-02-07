ed4beb29d283f408ff0bd8e8d89e00f4218d79fb
Utkarsh_singh07 <singhutkarsh304@gmail.com>
2023-02-08 00:06:24 +0530

    Delete shorturl.py

--- shorturl.py
+++
@@ -1,9 +0,0 @@
-def to_base_62(deci):
-    s = '012345689abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    hash_str = ''
-    while deci > 0:
-       hash_str= s[deci % 62] + hash_str
-       deci /= 62
-    return hash_str
-  
-print to_base_62(999)
